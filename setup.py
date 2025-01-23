9dde6cc4a31007b8568ed508223581d549493b6c
datalayer0 <hello@chunk.limo>
2025-01-23 07:29:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="chat",
     version="0.1.0",
-    packages=find_packages(include=["backend", "libs"]),
+    packages=["backend"],
     install_requires=[
         "chainlit",
         "fastapi",
         "uvicorn"
-    ]
+    ],
+    exclude=["frontend", "cypress", "images", "node_modules"]
 )
